9b003e4dccc37505c092b9ebfb12dc0c1abff1e9
BuildTools <fusion.lightcat+github@gmail.com>
2017-01-16 10:53:28 +0100

    first interface of new command framework

---
+++ src/main/java/io/github/cyborgnoodle/chatcli/Command.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017 Enveed / Arthur Schüler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.cyborgnoodle.chatcli;
+
+import de.btobastian.javacord.entities.Channel;
+import de.btobastian.javacord.entities.User;
+import de.btobastian.javacord.entities.message.Message;
+import io.github.cyborgnoodle.CyborgNoodle;
+
+/**
+ *
+ */
+public abstract class Command {
+
+    private Message message;
+    private Channel channel;
+    private User author;
+
+    private CyborgNoodle noodle;
+
+    public void execute(Message message, String[] args){
+
+        this.message = message;
+        this.channel = message.getChannelReceiver();
+        this.author = message.getAuthor();
+
+        if(emptyHelp() && args.length==0){
+            channel.sendMessage("Invalid Arguments! Usage: `"+usage()+"`");
+        }
+        else onCommand(args);
+    }
+
+    public abstract void onCommand(String[] args);
+
+    public abstract String[] aliases();
+
+    public abstract String usage();
+
+    public abstract boolean emptyHelp();
+
+    public Command(CyborgNoodle noodle){
+        this.noodle = noodle;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public CyborgNoodle getNoodle() {
+        return noodle;
+    }
+}
